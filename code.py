902d0f5379f88d8234da2ef6e3603519d1fbcd50
Mohsan Ali <mosanjoyia45@gmail.com>
2024-09-14 23:22:15 +0500

    s

---
+++ code.py
@@ -0,0 +1,43 @@
+# 1. Basic Printing:
+a="Hello, world!"
+print(a)
+b="Mohsan"
+print(b)
+
+# 2. Printing Multiple Items:
+
+c="ali"
+
+print(f'{b} {c}')
+print(b,c)
+
+print('1 2 3')
+
+# 3. Printing Special Characters:
+print('hello /n word')
+
+print('HelloWord')
+
+# 4. Using the sep Parameter:
+
+print('apple, banana, cherry')
+print('1-2-3')
+# 5. Using the end Parameter:
+print('Hello World')
+print('12')
+
+# 6. Escape Characters:
+print('He said, "Hello!"')
+print('\\')
+
+# 7. Combining Text and Numbers:
+print('I am a 20 years old')
+d=5
+print(f'The number is {d}')
+
+# 8. Basic Loop for Printing:
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
